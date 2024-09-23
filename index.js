db907a942ef1b719817675be203ed5b7c5f2d69c
Patrick Kipkoech <patrickwayy@gmail.com>
2024-09-23 09:44:29 +0300

    Updates on index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards(names, event){
+    let thanYouCard = [];
+    for (let i = 0; i < names.length; i++){
+        thanYouCard.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return thanYouCard;
+}
+
+function countDown(startingNumber){
+    while (startingNumber>= 0){
+        console.log(startingNumber);
+        startingNumber--;
+    }
+}
+let name = ["Guadalupe","Ollie","Aki"];
+let event = "surprise"
+
+console.log(writeCards(name,event ));
+countDown(5)
